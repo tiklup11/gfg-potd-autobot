221b0eea0119febd655ee1aa03dc7db144a6d37e
tiklupxgit <tiklup1729@gmail.com>
2023-03-09 03:17:38 +0530

    removed adminpass field

--- src/route.js
+++ src/route.js
@@ -1,79 +1,81 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const bodyParser = require('body-parser')
-const router = express.Router()
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const router = express.Router();
 
-const db_service = require('./services/firestore')
-const formatter = require('./format')
+const db_service = require("./services/firestore");
+const formatter = require("./format");
 
-router.use(express.static(path.join(__dirname, 'public')));
+router.use(express.static(path.join(__dirname, "public")));
 
-router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/adduser.html') // serve the HTML page
-})
-
-router.get('/update_cookie', (req, res) => {
-    res.sendFile(__dirname + '/update.html') // serve the HTML page
-})
-
-router.get('/styles.css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/styles.css'));
+router.get("/", (req, res) => {
+  res.sendFile(__dirname + "/adduser.html"); // serve the HTML page
 });
 
+router.get("/update_cookie", (req, res) => {
+  res.sendFile(__dirname + "/update.html"); // serve the HTML page
+});
 
-router.post('/adduser', async (req, res) => {
+router.get("/styles.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "/styles.css"));
+});
 
-    var { name, email, cookie, adminpassword, userpass } = req.body;
+router.post("/adduser", async (req, res) => {
+  var { name, email, cookie, userpass } = req.body;
 
-    if (checkValidations(name, email, cookie, userpass) === false) {
-        return res.send("All fields are required")
-    }
+  if (checkValidations(name, email, cookie, userpass) === false) {
+    return res.send("All fields are required");
+  }
 
-    if (isJson(cookie) === false) {
-        return res.send("Cookie must be in JSON format, use Cookie-editor extension to get cookies")
-    }
+  if (isJson(cookie) === false) {
+    return res.send(
+      "Cookie must be in JSON format, use Cookie-editor extension to get cookies"
+    );
+  }
 
-    if (adminpassword === "niceone") {
-        cookie = formatter.formatCookie(JSON.parse(cookie))
-        await db_service.addUserToFirestore({ name: name, email: email, cookie: cookie, userpass: userpass })
-        return res.send('User added')
-    } else {
-        return res.send("Admin Pass required")
-    }
-
-})
+  cookie = formatter.formatCookie(JSON.parse(cookie));
+  await db_service.addUserToFirestore({
+    name: name,
+    email: email,
+    cookie: cookie,
+    userpass: userpass,
+  });
+  return res.send("User added");
+});
 
 function checkValidations(name, email, cookie, userpass) {
-    if (name.trim() === "" || email.trim() === "" || cookie.trim() === "" || userpass.trim() === "") {
-        return false;
-    }
-    return true;
+  if (
+    name.trim() === "" ||
+    email.trim() === "" ||
+    cookie.trim() === "" ||
+    userpass.trim() === ""
+  ) {
+    return false;
+  }
+  return true;
 }
 
+router.post("/update_cookie", async (req, res) => {
+  var { email, cookie, userpass } = req.body;
 
-router.post('/update_cookie', async (req, res) => {
-
-    var { email, cookie, userpass } = req.body;
-
-    if (isJson(cookie) === false) {
-        return res.send("Cookie must be in JSON format, use Cookie-editor extension to get cookies")
-    }
-    return res.send(await db_service.updateCookie(cookie, email, userpass))
-
-})
+  if (isJson(cookie) === false) {
+    return res.send(
+      "Cookie must be in JSON format, use Cookie-editor extension to get cookies"
+    );
+  }
+  return res.send(await db_service.updateCookie(cookie, email, userpass));
+});
 
 function isJson(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
 }
 
-
-
-module.exports = router
+module.exports = router;
